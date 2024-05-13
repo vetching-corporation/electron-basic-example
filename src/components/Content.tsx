e353b458f609b0f98c1cbc475d816c4af407b5bd
drawhale <kdw115@naver.com>
2024-05-13 14:24:43 +0900

    chore: remove git cache

--- src/components/Content.tsx
+++
@@ -1,8 +0,0 @@
-export const Content = () => {
-  return (
-    <main>
-      <h2>Content</h2>
-      <p>This is the content of the application.</p>
-    </main>
-  );
-};
--- src/components/Header.tsx
+++
@@ -1,3 +0,0 @@
-export const Header = () => {
-  return <h1>Electon Example</h1>;
-};
